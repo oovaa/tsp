bf1ef27d9e2dd59dd03795995a46124bbe7a73dc
oovaa <omarvx211@gmail.com>
2024-07-31 10:58:12 +0200

    radis cache is now working at 31-10

--- index.js
+++ index.js
@@ -1,60 +1,72 @@
 import 'colors'
 import express from 'express'
-import redis from 'redis'
+import { createClient } from 'redis'
+import fetch from 'node-fetch' // Ensure node-fetch is imported
 
 const PORT = 5000
 const REDIS_PORT = 6379
 
-const client = redis.createClient(REDIS_PORT)
+const client = createClient({
+  url: `redis://localhost:${REDIS_PORT}`,
+})
 
 client.on('error', (err) => {
   console.error('Redis client error:', err)
 })
 
-client.connect().catch(console.error)
+await client.connect().catch(console.error)
 
 const app = express()
 
 function setRes(username, repos) {
-  return `${username} has ${repos} gh repos`
+  return `${username} has ${repos} GitHub repos`
 }
 
-function cache(req, res, next) {
+async function cache(req, res, next) {
   const { username } = req.params
-  client.get(username, (err, data) => {
-    if (err) throw err
-
+  try {
+    const data = await client.get(username)
     if (data !== null) res.send(setRes(username, data))
     else next()
-  })
-}
-
-async function getRepos(req, res, next) {
-  try {
-    console.log('fetching data...')
-    const { username } = req.params
-
-    const response = await fetch(`https://api.github.com/users/${username}`)
-    console.log('getting the response ...')
-
-    const data = await response.json()
-
-    console.log('getting the response to json ...')
-
-    const repos = data.public_repos
-
-    await client.setEx(username, 3600, repos.toString()) // Convert repos to string
-    console.log('setting the cache ...')
-
-    res.send(setRes(username, repos))
-  } catch (error) {
-    console.log(error)
+  } catch (err) {
+    console.error('Redis get error:', err)
     res.status(500).send('Internal Server Error')
   }
 }
 
+async function getRepos(req, res) {
+  try {
+    console.log('Fetching data...')
+    const { username } = req.params
+
+    const response = await fetch(`https://api.github.com/users/${username}`)
+    console.log('Getting the response...')
+
+    if (!response.ok) {
+      throw new Error(`GitHub API responded with status ${response.status}`)
+    }
+
+    const data = await response.json()
+    console.log('Parsing the response to JSON...')
+
+    const repos = data.public_repos
+
+    await client.setEx(username, 3600, repos.toString()) // Convert repos to string
+    console.log('Setting the cache...')
+
+    res.send(setRes(username, repos))
+  } catch (error) {
+    console.error('Error fetching repos:', error)
+    res.status(500).send('Internal Server Error')
+  }
+}
+
+app.get('/', (req, res) => {
+  res.send('Hi there')
+})
+
 app.get('/repos/:username', cache, getRepos)
 
 app.listen(PORT, () =>
-  console.log(`App is listing on port ${PORT} http://localhost:${PORT}`.blue)
+  console.log(`App is listening on port ${PORT} http://localhost:${PORT}`.blue)
 )
